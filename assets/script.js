53b1b1cb02bc20e4d9fe792efb5433dad8efc59b
Cris0338 <cris0338@gmail.com>
2023-11-02 00:19:37 +0100

    Carousel Added

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,46 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// index des slides à 0
+let currentSlide = 0;
+
+// Montre l'image
+function showSlide() {
+    // prend l'image
+    document.querySelector('.banner-img').src = "./assets/images/slideshow/" + slides[currentSlide].image;
+    // prend le tagline de l'image
+    document.querySelector('#banner p').innerHTML = slides[currentSlide].tagLine;
+    
+    // prend le containeur des dots
+    let dots = document.querySelector('.dots');
+    // reset des dots
+    dots.innerHTML = '';
+    // création du dot inerent à l'image
+    for(let i=0; i<slides.length; i++) {
+        let dot = document.createElement('span');
+        // si le dot correspond a l'image atribution de la classe 'dot_selected'
+        dot.className = 'dot' + (i === currentSlide ? ' dot_selected' : '');
+        // ajout du dot
+        dots.appendChild(dot);
+    }
+}
+
+// prend la fleche de gauche et ajout evenement
+document.querySelector('.arrow_left').addEventListener('click', function() {
+    // -1 ou dernier si 0
+    currentSlide = (currentSlide > 0 ? currentSlide : slides.length) - 1;
+    // montre l'img
+    showSlide();
+});
+
+// prend la fleche de droite et ajout evenement
+document.querySelector('.arrow_right').addEventListener('click', function() {
+    // +1 ou 0 si dernier
+    currentSlide = (currentSlide + 1) % slides.length;
+    // montre l'img
+    showSlide();
+});
+
+// première image (0) au chargement
+showSlide();
