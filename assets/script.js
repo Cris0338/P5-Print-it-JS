02bbf02d371d0cb26b9a373e9826ac94c38f7de6
Cris0338 <cris0338@gmail.com>
2023-11-05 13:45:08 +0100

    JS mod

--- assets/script.js
+++ assets/script.js
@@ -22,24 +22,19 @@ let currentSlide = 0;
 
 // Montre l'image
 function showSlide() {
-    // recherche la balise .banner_img, lui donne une source, lui attribue le tableau et lui indique qoi prendre dans le tableau
+    // recherche la balise .banner_img, lui donne une source, lui attribue le tableau et lui indique quoi prendre dans le tableau
     document.querySelector('.banner-img').src = "./assets/images/slideshow/" + slides[currentSlide].image;
     // recherche la balise .#banner p, lui dit de l'inserer, lui dit d'utiliser celle du slide utilisé
     document.querySelector('#banner p').innerHTML = slides[currentSlide].tagLine;
-    
-    // recherche la balise containeur des dots
-    let dots = document.querySelector('.dots');
-    // reset des dots
-    dots.innerHTML = '';
-    // génération des dots sur le comptages des elements du tableau
-    for(let i=0; i<slides.length; i++) {
-        // création du dot
-        let dot = document.createElement('span');
-        // attribution de la classe dot, + si le dot correspond === à l'image atribution de la classe 'dot_selected' autrement(:) vide
-        dot.className = 'dot' + (i === currentSlide ? ' dot_selected' : '');
-        // ajout du dot
-        dots.appendChild(dot);
-    }
+
+    // Selection tous les elements avec la classe "dot" et les memorise dans un tableau "dots"
+    let dots = document.querySelectorAll('.dot');
+
+    // Repéte sur chaque element du tableau "dots"
+    dots.forEach((dot, index) => {
+    // Si index est égale à currentSlide, ajoute la classe "dot_selected" : autrement enléve la classe "dot_selected"
+    dot.className = 'dot' + (index === currentSlide ? ' dot_selected' : '');
+});
 }
 
 // prend la fleche de gauche et ajout evenement
@@ -57,6 +52,22 @@ document.querySelector('.arrow_right').addEventListener('click', function() {
     // montre l'img
     showSlide();
 });
+
+// Genére les dot au chargement du DOM
+// Selection de l'element avec la classe "dots" et crée la variabile "dotsContainer"
+let dotsContainer = document.querySelector('.dots');
+
+// Crée avec la repetion pour chaque slide avec for de 0 à slides.length
+for(let i = 0; i < slides.length; i++) {
+    // Crée un dot pour chaque "span"
+    let dot = document.createElement('span');
+
+    // Crée la classe "dot" de la variable "dot"
+    dot.className = 'dot';
+
+    // Ajoute "dot" comme child dans "dotsContainer"
+    dotsContainer.appendChild(dot);
+}
 
 // première image (0) au chargement
 showSlide();
